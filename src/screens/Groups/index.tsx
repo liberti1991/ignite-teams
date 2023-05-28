a3049bd419803fe77f65a20cc2de06ad6e31c154
Rodrigo <liberti1991@gmail.com>
2023-05-28 15:21:16 -0300

    projeto teams

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { stylesGroups } from "./styles";
+
+export function Groups() {
+  return (
+    <View style={stylesGroups.container}>
+      <Text>Oi</Text>
+    </View>
+  );
+}
